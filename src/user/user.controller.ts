eb7d8a9c592574a1b055da458fc3057ffcc3a6c7
Abah Z <zaunaf@yahoo.com>
2021-01-18 09:35:43 +0700

    Bismillah.. first commit, 001-user-crud

---
+++ src/user/user.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { User } from './user.interface';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  create(@Body() user: User): Observable<User> {
+    console.log(user);
+    return this.userService.create(user);
+  }
+
+  @Get(':id')
+  findOne(@Param() params): Observable<User> {
+    return this.userService.findOne(params.id);
+  }
+
+  @Get()
+  findAll(): Observable<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Delete(':id')
+  deleteOne(@Param('id') id: string): Observable<User> {
+    return this.userService.deleteOne(Number(id));
+  }
+
+  @Put(':id')
+  updateOne(@Param('id') id: string, @Body() user: User): Observable<User> {
+    return this.userService.updateOne(Number(id), user);
+  }
+}
